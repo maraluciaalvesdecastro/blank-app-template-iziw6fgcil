7d019065992b2de38c68c152d31b4cf9f4733182
Mara Castro <162910891+maraluciaalvesdecastro@users.noreply.github.com>
2024-07-11 01:01:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Solare app")
+st.title(" Solare app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
